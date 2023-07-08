dab018c9a47237da07116fdee1c93af080e72e1d
PietroDSE <138404232+PietroDSE@users.noreply.github.com>
2023-07-08 02:08:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+window.alert("Minha primeira mensagem")
+window.confirm("Você gosta de JavaScript?")
+window.prompt("qual seu nome")
+
+
+
+
+
+var n1 =5
+var n2 =8.5
+var n3 =15
+//variáveis com números
+//variáveis com palavras
+var s1 ='JavaScript'
+var s2 ='DAvila'
+var s3 ='Guanabara'
+
+
+
+
+/*
+variáveis podem começar com letras, $ ou _
+Ex: N1, $js, _Guanabara 
+não podem começar com números
+Ex: 1n
+é possível misturar letras e números
+Ex: N1, S1
+é possível usar assentos e simbolos
+Ex: média, €uro
+não podem conter espaços
+Ex: Olá mundo. substitua o espaço por _
+nâo podem ser nomes de comandos
+EX: function, whole, var, prompt
+
+Dicas:
+maiusculas e minúsculas fazem diferença
+EX: N1 e n1 são duas variáveis diferentes
+utilize nomes coerentes
+EX: guardar nomes = nome, salário = sal ou salário
+
+tipos primitivos:
+ 5 - 18 - -12 - 0.5 - -15.9 - 3.14 - 8.0
+ number
+ "google", 'Javascript', `neymar`
+ string
+ true, false
+ boolean
+*/
+  
