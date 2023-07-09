3b0c1560f5a894304482c657d8c6db9f2d942f16
Pietro d'Avila <dsescuderop@gmail.com>
2023-07-09 17:49:34 -0300

    Js-2

---
+++ ex-002/script.js
@@ -0,0 +1,12 @@
+var n1 = Number(window.prompt("digite um número"))
+var n2 = Number(window.prompt("digite outro número"))
+//em versões recentes temos number, parseInt, parseFloat e Number
+var s =  n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é igual à ${s}`)
+//+ para adição
+//+ para concatenação
+//aspas e aspas duplas para strings simples
+//crase `` para ${}
+var nome = 'Pietro'
+var idade = 13
+var nota = 5.5
