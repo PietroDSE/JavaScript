98f5b40d811f4c9ea5417513bfc7d92621f842c1
Pietro d'Avila <dsescuderop@gmail.com>
2023-07-09 21:15:20 -0300

    sdsad

--- ex-003/script.js
+++ ex-003/script.js
@@ -2,5 +2,5 @@
 
 var n1 =Number(prompt('qual o preço do produto?'))
 
-document.writeln(`o produto custa ${n1.toFixed(2).replace('.', ','.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))}`)
+document.writeln(`o produto custa ${n1.toFixed(2).replace('.', ',').toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`)
 
---
+++ ex-004/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('qual seu nome')
+document.write(`olá ${nome}`)
